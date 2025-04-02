f6a8e1c4a6c4d95af6e4d05089a4fc207565a876
ParkChangSun <changsun19991212@gmail.com>
2025-04-03 00:30:13 +0900

    fix: signup kr localize

--- handlers/RestApi/SignUpFunc/main.go
+++ handlers/RestApi/SignUpFunc/main.go
@@ -19,13 +19,13 @@ func handler(ctx context.Context, req events.APIGatewayProxyRequest) (events.API
 
 	_, err := ddb.GetUser(ctx, body.Id)
 	if !errors.Is(err, ddb.ErrItemNotFound) {
-		b, _ := json.Marshal(auth.ErrorResponseBody{Message: "id exists"})
+		b, _ := json.Marshal(auth.ErrorResponseBody{Message: "아이디 중복"})
 		return auth.RESTResponse(400, auth.CORSHeaders, string(b)), nil
 	}
 
 	idlen := regexp.MustCompile(`^[0-9a-zA-Z]{4,15}$`)
 	if !idlen.Match([]byte(body.Id)) {
-		b, _ := json.Marshal(auth.ErrorResponseBody{Message: "The id should contain a combination of 4 to 15 letters and numbers."})
+		b, _ := json.Marshal(auth.ErrorResponseBody{Message: "아이디 : 특수기호 없는 영문/숫자 4-15 글자"})
 		return auth.RESTResponse(400, auth.CORSHeaders, string(b)), nil
 	}
 
@@ -33,7 +33,7 @@ func handler(ctx context.Context, req events.APIGatewayProxyRequest) (events.API
 	eng := regexp.MustCompile(`[a-zA-Z]`)
 	bytelen := regexp.MustCompile(`^[a-zA-Z0-9@#$%^&*]{6,30}$`)
 	if !num.Match([]byte(body.Password)) || !eng.Match([]byte(body.Password)) || !bytelen.Match([]byte(body.Password)) {
-		b, _ := json.Marshal(auth.ErrorResponseBody{Message: "The password must contain a combination of 6 to 30 letters and numbers."})
+		b, _ := json.Marshal(auth.ErrorResponseBody{Message: "비밀번호 : 영문+숫자 6-30 글자 (포함가능 : @#$%^&*)"})
 		return auth.RESTResponse(400, auth.CORSHeaders, string(b)), nil
 	}
 	hash, _ := bcrypt.GenerateFromPassword([]byte(body.Password), bcrypt.DefaultCost)
